74d794da4ae780f1b33a1410e2cc966c3c057872
Ehsan Nouri <ehsanouri@gmail.com>
2024-05-23 13:59:19 +0300

    add fk

--- src/bot/handlers/invite.py
+++ src/bot/handlers/invite.py
@@ -25,6 +25,7 @@ async def invite_handler(message: Message, session_factory: async_sessionmaker[A
     caption = formatting.as_list(
     formatting.as_line(
         formatting.Spoiler(get_text(lang, "Telegram premium")),
+        formatting.Spoiler(get_text(lang, "$400 giveaway")),
         formatting.Spoiler(get_text(lang, "100% Share of ad revenue")),
         sep="\n",
     ),
