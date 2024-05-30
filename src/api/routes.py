600bb870f4a9336db5732e0c5f7690d983b86252
Ehsan Nouri <ehsanouri@gmail.com>
2024-05-30 14:30:50 +0300

    first commit

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
 
-from src.api.endpoints import stat_webhook, webhook
+from src.api.endpoints import stat_webhook, webhook, profile, lpush
 
 router = APIRouter()
-routers_list = (webhook.router, stat_webhook.router)
+routers_list = (webhook.router, stat_webhook.router, profile.router, lpush.router)
 
 for r in routers_list:
     router.include_router(r)
