b2efad1c4e2e1e3072bde808a997831cdc80e11e
Ehsan Nouri <ehsanouri@gmail.com>
2024-06-05 22:40:41 +0300

    remove unnnec if

--- src/tasks.py
+++ src/tasks.py
@@ -14,31 +14,35 @@ async def stat_task(stat_bot: Bot, session_factory: async_sessionmaker[AsyncSess
         user_repository = UserRepository(session)
         system_repository = SystemRepository(session)
         system_log_repository = SystemLogRepository(session)
+        try:
+            async with session.begin():
+                system = await system_repository.get()
+                all_u = await user_repository.get_users_order_by_join_and_limit(system.last_user_log, system.max_user_cumulative)
+                all_logs = await system_log_repository.get_logs_order_by_time_and_limit(system.last_action_log, system.max_user_cumulative * 3)
 
-        async with session.begin():
-            system = await system_repository.get()
-            all_u = await user_repository.get_users_order_by_join_and_limit(system.last_user_log, system.max_user_cumulative)
-            all_logs = await system_log_repository.get_logs_order_by_time_and_limit(system.last_action_log, system.max_user_cumulative * 3)
+                if all_u:
+                    system.last_user_log = all_u[-1].created_at
 
+                if all_logs:
+                    system.last_action_log = all_logs[-1].created_at
+
+                if all_u or all_logs:
+                    await system_repository.update(system)            
             if all_u:
-                system.last_user_log = all_u[-1].created_at
+                text = "\n\n".join([f"{i+1}. {user.full_info}" for i, user in enumerate(all_u)])
+                try:
+                    await stat_bot.send_message(chat_id=STAT_CHAT_ID, message_thread_id=JOIN_THREAD_ID, text=text)
+                except Exception as e:
+                    print(e)
 
             if all_logs:
-                system.last_action_log = all_logs[-1].created_at
-
-            if all_u or all_logs:
-                await system_repository.update(system)            
-        if all_u:
-            text = "\n\n".join([f"{i+1}. {user.full_info}" for i, user in enumerate(all_u)])
-            try:
-                await stat_bot.send_message(chat_id=STAT_CHAT_ID, message_thread_id=JOIN_THREAD_ID, text=text)
-            except Exception as e:
-                print(e)
-
-        if all_logs:
-            text = "\n\n".join([f"{i+1}. {log.created_at} command: {log.command} \n {log.user.info}" for i, log in enumerate(all_logs)])
-            try:
-                await stat_bot.send_message(chat_id=STAT_CHAT_ID, message_thread_id=USER_LOG_THREAD_ID, text=text)
-            except Exception as e:
-                print(e)
+                text = "\n\n".join([f"{i+1}. {log.created_at} command: {log.command} \n {log.user.info}" for i, log in enumerate(all_logs)])
+                try:
+                    await stat_bot.send_message(chat_id=STAT_CHAT_ID, message_thread_id=USER_LOG_THREAD_ID, text=text)
+                except Exception as e:
+                    print(e)
+        except Exception as e:
+            print(e) # todo send to system log
+        finally:
+            await session.close()
         await asyncio.sleep(60)
