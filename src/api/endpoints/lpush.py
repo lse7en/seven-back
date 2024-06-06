c8596ff052e8b9562b4f8bdd782b02c68271c8c5
Ehsan Nouri <ehsanouri@gmail.com>
2024-06-06 20:43:19 +0300

    remove unnnec if

--- src/api/endpoints/lpush.py
+++ src/api/endpoints/lpush.py
@@ -6,10 +6,11 @@ from src.deps import  CurrentUser
 from src.core.database import DBSession
 from src.schemas.user_schemas import User
 import random
-from datetime import datetime, UTC
+from datetime import datetime, UTC, timedelta
 from src.repositories.user_repository import UserRepository
 from src.repositories.system_log_repository import SystemLogRepository
 from src.models.system_log import SystemLog
+from
 router = APIRouter(prefix="/lpush", tags=["lpush"])
 
 
@@ -22,6 +23,14 @@ async def lpush(
 ):
     # generate random  between 1 and 20
     r = random.randint(1, 200)
+
+
+    minutes = (2 ** (3 - current_user.invited_users)) * 60
+
+    next_push = current_user.last_lucky_push + timedelta(minutes=minutes)
+
+    if datetime.now(UTC) < next_push:
+        return current_user
 
     async with session.begin():
         await system_log_repository.add_log(SystemLog(user=current_user, command="get:push"))
