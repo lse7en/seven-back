7ee4bc8932b72fecc3f7d23f0ab0bc3a36cba268
Ehsan Nouri <ehsanouri@gmail.com>
2024-06-27 20:51:30 +0300

    checking with ruff

--- src/api/endpoints/rank.py
+++ src/api/endpoints/rank.py
@@ -21,13 +21,16 @@ async def rank(
     rank = await user_repository.get_user_rank(current_user.id)
     # rank = current_user.static_rank
     current_user.rank = rank
-    await system_log_repository.add_log(SystemLog(user=current_user, command=f"rank:{rank}"))
-
-    if rank > 20:
-        min_invitations = await user_repository.get_min_invitation_count_for_rank(20)
-        min_points = await user_repository.get_min_points_for_rank(20)
-
-        current_user.min_invitations = min_invitations
-        current_user.min_points = min_points
-
+    # await system_log_repository.add_log(SystemLog(user=current_user, command=f"rank:{rank}"))
+    current_user.min_invitations = 0
+    current_user.min_points = 0
     return current_user
+
+    # if rank > 20:
+    #     min_invitations = await user_repository.get_min_invitation_count_for_rank(20)
+    #     min_points = await user_repository.get_min_points_for_rank(20)
+
+    #     current_user.min_invitations = min_invitations
+    #     current_user.min_points = min_points
+
+    # return current_user
