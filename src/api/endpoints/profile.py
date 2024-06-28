b10568d62dc0987a59651b2403cc5df438144d93
Ehsan Nouri <ehsanouri@gmail.com>
2024-06-28 10:52:44 +0300

    checking with ruff

--- src/api/endpoints/profile.py
+++ src/api/endpoints/profile.py
@@ -40,54 +40,16 @@ async def joined(
 
 
     async with user_repository.session.begin():
-        current_user = await user_repository.get_user_or_none_by_id(user_id)
+        current_user = await user_repository.get_user_for_update(user_id)
         current_user.joined = joined
-        await user_repository.add_user(current_user)
 
-
-    if joined and current_user.referrer_id and not current_user.referrer_score:
-
-        async with user_repository.session.begin():
-            referrer = await user_repository.get_user_or_none_by_id(current_user.referrer_id)
+        if joined and current_user.referrer_id and not current_user.referrer_score:
+            referrer = await user_repository.get_user_for_update(current_user.referrer_id)
             referrer.invited_users += 1
             referrer.points += 1000
-            current_user.referrer_score = True
             await user_repository.add_user(referrer)
-            await user_repository.add_user(current_user)
-
-
-        joined_message = formatting.as_list(
-            formatting.as_line(formatting.Bold(get_text(referrer.language, "Keep going!")), "💪", sep=" "),
-            formatting.as_line(
-                get_text(referrer.language, "Your friend"),
-                formatting.Bold(current_user.full_name),
-                get_text(referrer.language, "joined the Bot!"),
-                sep=" ",
-            ),
-            formatting.as_list(
-                get_text(referrer.language, "And You get 1000 points for that!"),
-            )
-            ,
-            formatting.as_list(
-                get_text(referrer.language, "You have invited {} friends.").format(referrer.invited_users),
-                formatting.as_line(
-                    get_text(referrer.language, "And you have gathered"),
-                    formatting.Italic(f"{referrer.points}"),
-                    get_text(referrer.language, "points so far!"),
-                    sep=" ",
-                ),
-            ),
-            sep="\n\n",
-        )
-
-        try:
-            await request.app.state.bot.send_photo(
-                chat_id=referrer.id,
-                photo=FRIEND_INVITE_FILE_ID,
-                caption=joined_message.as_html(),
-            )
-        except Exception as e:
-            print("kire kharg")
-            print(e)
+        
+        current_user.referrer_score = True
+        await user_repository.add_user(current_user)
 
     return current_user
