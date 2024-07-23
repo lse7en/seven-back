d2dec80840bd919d8e931e6a2bfcf7f8ebaf4907
Ehsan Nouri <ehsanouri@gmail.com>
2024-07-23 14:56:33 +0300

    add adstg

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
 
-from src.api.endpoints import stat_webhook, webhook, profile, lpush, rank, secret, lottery, auth
+from src.api.endpoints import stat_webhook, webhook, profile, lpush, rank, secret, lottery, auth, adsg
 
 router = APIRouter()
-routers_list = (webhook.router, stat_webhook.router, profile.router, lpush.router, rank.router, secret.router, lottery.router, auth.router)
+routers_list = (webhook.router, stat_webhook.router, profile.router, lpush.router, rank.router, secret.router, lottery.router, auth.router, adsg.router)
 
 for r in routers_list:
     router.include_router(r)
