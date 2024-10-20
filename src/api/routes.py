54de117eb6fe1acf90ce4f9eec411a18173c1577
Ehsan Nouri <ehsanouri@gmail.com>
2024-10-20 18:45:23 +0300

    log bg

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
 
-from src.api.endpoints import stat_webhook, webhook, profile, lpush, rank, secret, lottery, auth, adsg
+from src.api.endpoints import stat_webhook, webhook, profile, lpush, rank, secret, lottery, auth, adsg, friend_task
 
 router = APIRouter()
-routers_list = (webhook.router, stat_webhook.router, profile.router, lpush.router, rank.router, secret.router, lottery.router, auth.router, adsg.router)
+routers_list = (webhook.router, stat_webhook.router, profile.router, lpush.router, rank.router, secret.router, lottery.router, auth.router, adsg.router, friend_task.router)
 
 for r in routers_list:
     router.include_router(r)
